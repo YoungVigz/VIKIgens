374456923e7b9b87dd5360d0b2ffb85ded77a7ac
Gabriel Galeza <gabrielgaleza@gmail.com>
2019-04-23 01:43:47 +0200

    Update README.md

--- src/helpers.js
+++ src/helpers.js
@@ -101,13 +101,13 @@ const bodyParser = require('body-parser');
     
 const app = express();
     
-//app.use(express.static('public'));
+app.use(express.static('public'));
     
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
     
 app.use('/', (res, req) => {
-    res.send('Welcome in ' + process.env.npm_package_name + ' app.');
+    res.json({ name: process.env.npm_package_name });
 });
     
 module.exports = app;`;
