77a182e464a2c6446071187dd55d71c3a2464c6c
Gabriel Galeza <gabrielgaleza@gmail.com>
2019-04-23 21:34:26 +0200

    Bug fixed

--- src/helpers.js
+++ src/helpers.js
@@ -129,6 +129,7 @@ const bodyParser = require('body-parser');
 const app = express();
     
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
     
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
